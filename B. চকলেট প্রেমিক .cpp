6cf80f12296c55425cb7517d40b231dae5c8aa73
Abdur Rahim <rahimbd623@gmail.com>
2018-10-10 22:59:40 +0600

    Create B. চকলেট প্রেমিক .cpp

---
+++ B. চকলেট প্রেমিক .cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int x, y, sum;
+    scanf("%d %d", &x, &y);
+    sum=x+y;
+    if(sum>=20){
+        printf("Yes! Got it");
+    }
+    else{
+        printf("Bad luck Tipu Miah");
+    }
+}
